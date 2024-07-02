cbb7a0bfe4dc406d34a4bd90b7ea3738d757f6f5
Sutee Sudprasert <sutee.s@gmail.com>
2024-07-02 11:02:39 +0700

    use solidity 0.8.26

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/extensions/AccessControlEnumerable.sol"; 
--- contracts/NFT2.sol
+++ contracts/NFT2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 
 import "./ACL.sol";
